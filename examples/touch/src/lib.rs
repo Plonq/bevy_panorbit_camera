acb2d1d0efeb6193fc174d2f392dcc40c2ac4299
Huon Imberger <huon@plonq.org>
2024-02-01 18:00:00 +1100

    Update touch example

--- examples/touch/src/lib.rs
+++ examples/touch/src/lib.rs
@@ -2,8 +2,10 @@
 // type aliases tends to obfuscate code while offering no improvement in code cleanliness.
 #![allow(clippy::type_complexity)]
 
+use bevy::core_pipeline::clear_color::ClearColorConfig;
 use bevy::{
     input::touch::TouchPhase,
+    log::LogPlugin,
     prelude::*,
     window::{ApplicationLifetime, PrimaryWindow, WindowMode},
 };
@@ -13,14 +15,21 @@ use bevy_panorbit_camera::{ActiveCameraData, PanOrbitCamera, PanOrbitCameraPlugi
 #[bevy_main]
 fn main() {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-            resizable: false,
-            mode: WindowMode::BorderlessFullscreen,
-            ..default()
-        }),
-        ..default()
-    }))
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    resizable: false,
+                    mode: WindowMode::BorderlessFullscreen,
+                    ..default()
+                }),
+                ..default()
+            })
+            .set(LogPlugin {
+                filter: "warn,winit=error,bevy_panorbit_camera=debug".into(),
+                level: bevy::log::Level::DEBUG,
+            }),
+    )
     .add_plugins(PanOrbitCameraPlugin)
     .add_systems(Startup, setup_scene)
     .add_systems(Update, button_handler);
@@ -33,33 +42,6 @@ fn main() {
     app.run();
 }
 
-fn touch_camera(
-    windows: Query<&Window>,
-    mut touches: EventReader<TouchInput>,
-    mut camera: Query<&mut Transform, With<Camera3d>>,
-    mut last_position: Local<Option<Vec2>>,
-) {
-    let window = windows.single();
-
-    for touch in touches.read() {
-        if touch.phase == TouchPhase::Started {
-            *last_position = None;
-        }
-        if let Some(last_position) = *last_position {
-            let mut transform = camera.single_mut();
-            *transform = Transform::from_xyz(
-                transform.translation.x
-                    + (touch.position.x - last_position.x) / window.width() * 5.0,
-                transform.translation.y,
-                transform.translation.z
-                    + (touch.position.y - last_position.y) / window.height() * 5.0,
-            )
-            .looking_at(Vec3::ZERO, Vec3::Y);
-        }
-        *last_position = Some(touch.position);
-    }
-}
-
 /// set up a simple 3D scene
 fn setup_scene(
     mut commands: Commands,
@@ -68,93 +50,98 @@ fn setup_scene(
     mut active_cam: ResMut<ActiveCameraData>,
     windows: Query<&Window, With<PrimaryWindow>>,
 ) {
-    // plane
+    // Ground
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Plane::from_size(5.0).into()),
-        material: materials.add(Color::rgb(0.6, 0.1, 0.2).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
-    // cube
+    // Cube
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.5, 0.4, 0.3).into()),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
     });
-    // sphere
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(
-            Mesh::try_from(shape::Icosphere {
-                subdivisions: 4,
-                radius: 0.5,
-            })
-            .unwrap(),
-        ),
-        material: materials.add(Color::rgb(0.1, 0.4, 0.8).into()),
-        transform: Transform::from_xyz(1.5, 1.5, 1.5),
+    // Light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
     });
-    // light
-    commands.spawn(PointLightBundle {
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        point_light: PointLight {
-            intensity: 5000.0,
-            // Shadows makes some Android devices segfault, this is under investigation
-            // https://github.com/bevyengine/bevy/issues/8214
-            #[cfg(not(target_os = "android"))]
-            shadows_enabled: true,
+    // Camera
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 1.5, 5.0)),
+            ..default()
+        },
+        PanOrbitCamera::default(),
+    ));
+    // 2D camera for debugging
+    commands.spawn(Camera2dBundle {
+        camera: Camera {
+            // Renders the minimap camera after the main camera, so it is rendered on top
+            order: 1,
+            ..default()
+        },
+        camera_2d: Camera2d {
+            clear_color: ClearColorConfig::None,
             ..default()
         },
         ..default()
     });
     // camera
-    let pan_orbit_id = commands
-        .spawn((
-            Camera3dBundle {
-                transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-                ..default()
-            },
-            PanOrbitCamera::default(),
-        ))
-        .id();
+    // let pan_orbit_id = commands
+    //     .spawn((
+    //         Camera3dBundle {
+    //             transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+    //             ..default()
+    //         },
+    //         PanOrbitCamera::default(),
+    //     ))
+    //     .id();
 
     // Test ui
-    commands
-        .spawn(ButtonBundle {
-            style: Style {
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                position_type: PositionType::Absolute,
-                left: Val::Px(50.0),
-                right: Val::Px(50.0),
-                bottom: Val::Px(50.0),
-                ..default()
-            },
-            ..default()
-        })
-        .with_children(|b| {
-            b.spawn(
-                TextBundle::from_section(
-                    "Test Button",
-                    TextStyle {
-                        font_size: 30.0,
-                        color: Color::BLACK,
-                        ..default()
-                    },
-                )
-                .with_text_alignment(TextAlignment::Center),
-            );
-        });
-
-    let primary_window = windows
-        .get_single()
-        .expect("There is only ever one primary window");
-    active_cam.set_if_neq(ActiveCameraData {
-        entity: Some(pan_orbit_id),
-        viewport_size: Some(Vec2::new(primary_window.width(), primary_window.height())),
-        window_size: Some(Vec2::new(primary_window.width(), primary_window.height())),
-        manual: true,
-    });
+    // commands
+    //     .spawn(ButtonBundle {
+    //         style: Style {
+    //             justify_content: JustifyContent::Center,
+    //             align_items: AlignItems::Center,
+    //             position_type: PositionType::Absolute,
+    //             left: Val::Px(50.0),
+    //             right: Val::Px(50.0),
+    //             bottom: Val::Px(50.0),
+    //             ..default()
+    //         },
+    //         ..default()
+    //     })
+    //     .with_children(|b| {
+    //         b.spawn(
+    //             TextBundle::from_section(
+    //                 "Test Button",
+    //                 TextStyle {
+    //                     font_size: 30.0,
+    //                     color: Color::BLACK,
+    //                     ..default()
+    //                 },
+    //             )
+    //             .with_text_alignment(TextAlignment::Center),
+    //         );
+    //     });
+    //
+    // let primary_window = windows
+    //     .get_single()
+    //     .expect("There is only ever one primary window");
+    // active_cam.set_if_neq(ActiveCameraData {
+    //     entity: Some(pan_orbit_id),
+    //     viewport_size: Some(Vec2::new(primary_window.width(), primary_window.height())),
+    //     window_size: Some(Vec2::new(primary_window.width(), primary_window.height())),
+    //     manual: true,
+    // });
 }
 
 fn button_handler(
