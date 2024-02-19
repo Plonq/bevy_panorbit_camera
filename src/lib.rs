15ccd951e86e430163c2754965e601933572ad42
Huon Imberger <huon@plonq.org>
2024-02-19 20:55:16 +1100

    Add more parallellism

--- src/lib.rs
+++ src/lib.rs
@@ -46,9 +46,12 @@ impl Plugin for PanOrbitCameraPlugin {
             .add_systems(
                 Update,
                 (
-                    active_viewport_data
-                        .run_if(|active_cam: Res<ActiveCameraData>| !active_cam.manual),
-                    (mouse_key_tracker, touch_tracker),
+                    (
+                        active_viewport_data
+                            .run_if(|active_cam: Res<ActiveCameraData>| !active_cam.manual),
+                        mouse_key_tracker,
+                        touch_tracker,
+                    ),
                     pan_orbit_camera,
                 )
                     .chain()
@@ -375,6 +378,10 @@ fn active_viewport_data(
                         .expect("Must exist, since the camera is referencing it"),
                 };
 
+                // Is the cursor/touch in this window?
+                // Note: there's a bug in winit that causes `window.cursor_position()` to return
+                // a `Some` value even if the cursor is not in this window, in very specific cases.
+                // See: https://github.com/Plonq/bevy_panorbit_camera/issues/22
                 if let Some(input_position) = window.cursor_position().or(touches
                     .iter_just_pressed()
                     .collect::<Vec<_>>()
