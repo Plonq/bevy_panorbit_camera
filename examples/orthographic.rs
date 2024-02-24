b0d92ece071243d36d8ef5ddde60ddb589fe4857
Fpgu <35809384+341101@users.noreply.github.com>
2024-02-24 16:21:10 +0800

    Remove `scale` field from `PanOrbitCamera` (#58)

--- examples/orthographic.rs
+++ examples/orthographic.rs
@@ -9,6 +9,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(PanOrbitCameraPlugin)
         .add_systems(Startup, setup)
+        .add_systems(Update, switch_projection)
         .run();
 }
 
@@ -17,6 +18,17 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    // help
+    commands.spawn(TextBundle {
+        text: Text {
+            sections: vec![TextSection {
+                value: "Press R to switch projection".to_string(),
+                ..Default::default()
+            }],
+            ..Default::default()
+        },
+        ..default()
+    });
     // Ground
     commands.spawn(PbrBundle {
         mesh: meshes.add(Plane3d::default().mesh().size(5.0, 5.0)),
@@ -42,18 +54,27 @@ fn setup(
     // Camera
     commands.spawn((
         Camera3dBundle {
-            // Put the camera further away so clipping is less likely
-            transform: Transform::from_translation(Vec3::new(0.0, 5.5, 15.0)),
+            transform: Transform::from_translation(Vec3::new(0.0, 1.5, 6.0)),
             projection: Projection::Orthographic(OrthographicProjection {
-                scaling_mode: ScalingMode::FixedVertical(2.0),
+                scaling_mode: ScalingMode::FixedVertical(1.0),
                 ..default()
             }),
             ..default()
         },
-        PanOrbitCamera {
-            // Setting scale here will override the camera projection's initial scale
-            scale: Some(2.5),
-            ..default()
-        },
+        PanOrbitCamera::default(),
     ));
+}
+
+fn switch_projection(
+    mut next_projection: Local<Projection>,
+    key_input: Res<ButtonInput<KeyCode>>,
+    mut camera_query: Query<(&mut PanOrbitCamera, &mut Projection)>,
+) {
+    if key_input.just_pressed(KeyCode::KeyR) {
+        let Ok((mut camera, mut projection)) = camera_query.get_single_mut() else {
+            return;
+        };
+        std::mem::swap(&mut *next_projection, &mut *projection);
+        camera.force_update = true;
+    }
 }
