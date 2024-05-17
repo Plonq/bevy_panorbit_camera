2d82e21ad04b71a2c48a4aef2a946b8621314b34
Huon Imberger <huon@plonq.org>
2024-05-17 18:54:17 +1000

    revert example changes

--- examples/egui.rs
+++ examples/egui.rs
@@ -4,25 +4,18 @@
 use bevy::ecs::event::EventUpdateSignal;
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
-use bevy_panorbit_camera::{EguiWantsFocus, PanOrbitCamera, PanOrbitCameraPlugin};
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins)
-        // .insert_resource(EguiWantsFocus {
-        //     include_hover: true,
-        //     ..default()
-        // })
         .add_plugins(EguiPlugin)
         .add_plugins(PanOrbitCameraPlugin)
         .add_systems(Startup, setup)
-        .add_systems(
-            Update,
-            (draw_file_tree, draw_title_bar, ui_example_system).chain(),
-        );
+        .add_systems(Update, ui_example_system);
 
     // Make Bevy drop unconsumed events every frame to prevent weird behaviour when moving mouse
-    // out of an egui window immediately after scrolling (zooming)
+    // out of an egui window right after scrolling (zooming)
     // See: https://bevyengine.org/news/bevy-0-13/#events-live-longer
     app.world.remove_resource::<EventUpdateSignal>();
 
@@ -70,51 +63,4 @@ fn ui_example_system(mut contexts: EguiContexts) {
     egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
         ui.label("world");
     });
-}
-
-pub fn draw_title_bar(mut contexts: EguiContexts) {
-    egui::TopBottomPanel::top("title_bar").show(contexts.ctx_mut(), |ui| {
-        ui.visuals_mut().button_frame = false;
-        ui.horizontal(|ui| {
-            ui.menu_button("File", |ui| {
-                let _ = ui.button("Open").clicked();
-            });
-            ui.separator();
-            ui.menu_button("View", |ui| {
-                let mut test = false;
-                ui.checkbox(&mut test, "Checkbox A");
-                ui.checkbox(&mut test, "Checkbox B");
-            });
-        });
-    });
-}
-
-pub fn draw_file_tree(mut contexts: EguiContexts) {
-    egui::SidePanel::left("file_tree")
-        .default_width(300.0)
-        .max_width(500.0)
-        .show(contexts.ctx_mut(), |ui| {
-            egui::ScrollArea::both().auto_shrink(false).show(ui, |ui| {
-                egui::CollapsingHeader::new("Very Long Folder Name To Force Scroll A").show(
-                    ui,
-                    |ui| {
-                        ui.add(
-                            egui::Label::new("Very Long File Name To Force Scroll A")
-                                .selectable(false)
-                                .sense(egui::Sense::click()),
-                        );
-                        ui.add(
-                            egui::Label::new("Very Long File Name To Force Scroll B")
-                                .selectable(false)
-                                .sense(egui::Sense::click()),
-                        );
-                    },
-                );
-                ui.add(
-                    egui::Label::new("Very Long File Name To Force Scroll C")
-                        .selectable(false)
-                        .sense(egui::Sense::click()),
-                );
-            });
-        });
 }
