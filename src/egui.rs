05fe7f8085572961121133710a968f8c053f73c3
Huon Imberger <huon@plonq.org>
2024-07-09 18:03:30 +1000

    Fix panic if egui context for window doesn't exist

--- src/egui.rs
+++ src/egui.rs
@@ -39,12 +39,15 @@ pub fn check_egui_wants_focus(
     // that if any of the egui contexts want focus, then it must be the one that the user is
     // interacting with.
     let new_wants_focus = windows.iter().any(|window| {
-        let ctx = contexts.ctx_for_window_mut(window);
-        let mut value = ctx.wants_pointer_input() || ctx.wants_keyboard_input();
-        if include_hover.0 {
-            value |= ctx.is_pointer_over_area()
+        if let Some(ctx) = contexts.try_ctx_for_window_mut(window){
+            let mut value = ctx.wants_pointer_input() || ctx.wants_keyboard_input();
+            if include_hover.0 {
+                value |= ctx.is_pointer_over_area()
+            }
+            value
+        } else {
+            false
         }
-        value
     });
     let new_res = EguiWantsFocus {
         prev: wants_focus.curr,
