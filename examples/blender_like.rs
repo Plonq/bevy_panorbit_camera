fe577eff7673f5d2148931d3e85114a3abdc0a37
nate mccoy <nathan.mccoy@gmail.com>
2025-03-25 19:34:41 -0400

    Remove unused `TouchControls` and clean up comments
    The `TouchControls` field was removed as it is no longer used.

--- examples/blender_like.rs
+++ examples/blender_like.rs
@@ -6,7 +6,7 @@
 //!   Zoom: Mousewheel, ControlLeft + trackpad scroll
 
 use bevy::prelude::*;
-use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin, TouchControls, TrackpadBehavior};
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin, TrackpadBehavior};
 use std::f32::consts::TAU;
 
 fn main() {
@@ -46,22 +46,18 @@ fn setup(
         // Note we're setting the initial position below with yaw, pitch, and radius, hence
         // we don't set transform on the camera.
         PanOrbitCamera {
-            // Set focal point (what the camera should look at)
+            // // Set focal point (what the camera should look at)
             focus: Vec3::new(0.0, 1.0, 0.0),
-            // Set the starting position, relative to focus (overrides camera's transform).
+            // // Set the starting position, relative to focus (overrides camera's transform).
             yaw: Some(TAU / 8.0),
             pitch: Some(TAU / 8.0),
             radius: Some(5.0),
-            // Allow the camera to go upside down
+            // // Allow the camera to go upside down
             allow_upside_down: true,
             // Change the controls (these match Blender)
             button_orbit: MouseButton::Middle,
             button_pan: MouseButton::Middle,
             modifier_pan: Some(KeyCode::ShiftLeft),
-            // Reverse the zoom direction
-            reversed_zoom: false,
-            // Use alternate touch controls
-            touch_controls: TouchControls::TwoFingerOrbit,
             // camera responds to trackpad as in Blender
             // if you don't want to write out both modifiers,
             // you can use this helper method:
